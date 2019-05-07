42d763e353e9d14001a5003bde129dd81d056a24
Cory Schadt <cory@inputlogic.ca>
2019-05-07 14:01:28 -0700

    Enable django admin

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -45,7 +45,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    """objects = UserProfileManager()"""
+    objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
